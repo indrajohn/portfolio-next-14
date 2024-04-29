6cfcbdade5e040a46f8f652ec21887b35f733966
indrajohn <indrajohn2@gmail.com>
2024-04-29 11:57:35 +1000

    fix chat api

--- src/app/api/chat/route.js
+++ src/app/api/chat/route.js
@@ -1,133 +1,16 @@
-import { getVectorStore } from "@/lib/astradb";
-import { AIMessage, HumanMessage } from "@langchain/core/messages";
-import {
-  ChatPromptTemplate,
-  MessagesPlaceholder,
-  PromptTemplate,
-} from "@langchain/core/prompts";
-import { ChatOpenAI } from "@langchain/openai";
-import { Redis } from "@upstash/redis";
-import {
-  LangChainStream,
-  StreamingTextResponse,
-  Message as VercelChatMessage,
-} from "ai";
-import { UpstashRedisCache } from "@langchain/community/caches/upstash_redis";
-import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
-import { createHistoryAwareRetriever } from "langchain/chains/history_aware_retriever";
-import { createRetrievalChain } from "langchain/chains/retrieval";
-// export async function POST(request) {
-//   try {
-//     const body = await request.json();
-//     const messages = body.messages;
-
-//     const retriever = (await getVectorStore()).asRetriever();
-
-//     const chatHistory = messages
-//       .slice(0, -1)
-//       .map((m) =>
-//         m.role === "user"
-//           ? new HumanMessage(m.content)
-//           : new AIMessage(m.messages)
-//       );
-
-//     const currentMessageContent = messages[messages.length - 1].content;
-
-//     const cache = new UpstashRedisCache({ client: Redis.fromEnv() });
-
-//     const { stream, handlers } = LangChainStream();
-
-//     const chatModel = new ChatOpenAI({
-//       modelName: "gpt-3.5-turbo",
-//       streaming: true,
-//       callbacks: [handlers],
-//       verbose: true,
-//       cache,
-//     });
-
-//     const rephrashingModel = new ChatOpenAI({
-//       modelName: "gpt-3.5-turbo",
-//       verbose: true,
-//       cache,
-//     });
-
-//     const rephrasePrompt = ChatPromptTemplate.fromMessages([
-//       new MessagesPlaceholder("chat_history"),
-//       ["user", "{input}"],
-//       [
-//         "user",
-//         "Given the above conversation, generate a search query to look up in order to get information relevant to the current question. " +
-//           "Don't leave out any relevant keywords. Only return the query and no other text. if no result return no result",
-//         ,
-//       ],
-//     ]);
-
-//     const historyAwareRetrieverChain = await createHistoryAwareRetriever({
-//       llm: rephrashingModel,
-//       retriever,
-//       rephrasePrompt,
-//     });
-
-//     const prompt = ChatPromptTemplate.fromMessages([
-//       [
-//         "system",
-//         "You are tasked with operating as a chatbot for a personal portfolio website, " +
-//           "where your primary function is to impersonate the site's owner. " +
-//           "This unique role requires you to respond to inquiries in a manner that suggests " +
-//           "you are the owner of the website. It's essential to fully adopt the persona of the " +
-//           "website's proprietor during interactions. Please note that when providing answers " +
-//           "to users, you should refrain from including links ending in .js. Links should only be " +
-//           "used to direct users to specific areas of the portfolio for detailed insights. " +
-//           "Additionally, all your responses should be formatted using Markdown syntax. " +
-//           "Your objective is to engage with users based on the provided context, ensuring " +
-//           "a seamless and informative experience." +
-//           "Context:\n{context}",
-//       ],
-//       new MessagesPlaceholder("chat_history"),
-//       ["user", "{input}"],
-//     ]);
-
-//     const combineDocsChain = await createStuffDocumentsChain({
-//       llm: chatModel,
-//       prompt,
-//       documentPrompt: PromptTemplate.fromTemplate(
-//         "Page URL: {url}\n\nPage content:\n{page_content}"
-//       ),
-//       documentSeparator: "\n-------\n",
-//     });
-
-//     const retriverChain = await createRetrievalChain({
-//       combineDocsChain,
-//       retriever: historyAwareRetrieverChain,
-//     });
-//     retriverChain.invoke({
-//       input: currentMessageContent,
-//       chat_history: chatHistory,
-//     });
-
-//     return new StreamingTextResponse(stream);
-//   } catch (err) {
-//     console.log(err);
-//     return new StreamingTextResponse([
-//       "I am sorry, I couldn't find any relevant information about that.",
-//     ]);
-//     //return Response.json({ error: "Internal Server Error" }, { status: 500 });
-//   }
-// }
-
 export async function POST(request) {
   try {
     const body = await request.json();
+    console.log("Received request body:", body);
     const messages = body.messages;
-    console.log("message history: ", body);
 
-    const chatHistory = messages
-      .slice(0, -1)
-      .map((m) =>
-        m.role === "user"
-          ? new HumanMessage(m.content)
-          : new AIMessage(m.content)
-      );
+    if (!messages || !Array.isArray(messages) || messages.length === 0) {
+      return Response.json({ error: "Invalid message format" }, { status: 400 });
+    }
+
+    const chatHistory = messages.slice(0, -1).map((m) =>
+      m.role === "user" ? new HumanMessage(m.content) : new AIMessage(m.content)
+    );
 
     const currentMessageContent = messages[messages.length - 1].content;
 
@@ -145,12 +28,6 @@ export async function POST(request) {
       cache,
     });
 
-    const rephrasingModel = new ChatOpenAI({
-      modelName: "gpt-3.5-turbo",
-      verbose: true,
-      cache,
-    });
-
     const retriever = (await getVectorStore()).asRetriever();
 
     const rephrasePrompt = ChatPromptTemplate.fromMessages([
@@ -159,12 +36,12 @@ export async function POST(request) {
       [
         "user",
         "Given the above conversation, generate a search query to look up in order to get information relevant to the current question. " +
-          "Don't leave out any relevant keywords. Only return the query and no other text.",
+        "Don't leave out any relevant keywords. Only return the query and no other text.",
       ],
     ]);
 
     const historyAwareRetrieverChain = await createHistoryAwareRetriever({
-      llm: rephrasingModel,
+      llm: chatModel,
       retriever,
       rephrasePrompt,
     });
@@ -173,16 +50,16 @@ export async function POST(request) {
       [
         "system",
         "You are tasked with operating as a chatbot for a personal portfolio website, " +
-          "where your primary function is to impersonate the site's owner. " +
-          "This unique role requires you to respond to inquiries in a manner that suggests " +
-          "you are the owner of the website. It's essential to fully adopt the persona of the " +
-          "website's proprietor during interactions. Please note that when providing answers " +
-          "to users, you should refrain from including links ending in .js. Links should only be " +
-          "used to direct users to specific areas of the portfolio for detailed insights. " +
-          "Additionally, all your responses should be formatted using Markdown syntax. " +
-          "Your objective is to engage with users based on the provided context, ensuring " +
-          "a seamless and informative experience.\n\n" +
-          "Context:\n{context}",
+        "where your primary function is to impersonate the site's owner. " +
+        "This unique role requires you to respond to inquiries in a manner that suggests " +
+        "you are the owner of the website. It's essential to fully adopt the persona of the " +
+        "website's proprietor during interactions. Please note that when providing answers " +
+        "to users, you should refrain from including links ending in .js. Links should only be " +
+        "used to direct users to specific areas of the portfolio for detailed insights. " +
+        "Additionally, all your responses should be formatted using Markdown syntax. " +
+        "Your objective is to engage with users based on the provided context, ensuring " +
+        "a seamless and informative experience.\n\n" +
+        "Context:\n{context}",
       ],
       new MessagesPlaceholder("chat_history"),
       ["user", "{input}"],
@@ -208,7 +85,7 @@ export async function POST(request) {
     });
     return new StreamingTextResponse(stream);
   } catch (error) {
-    console.error(error);
+    console.error("Error processing request:", error);
     return Response.json({ error: "Internal server error" }, { status: 500 });
   }
 }
