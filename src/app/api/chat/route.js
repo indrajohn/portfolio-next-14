bc0f03efdbe8c5c9e413e409026a9f6f1c6e3383
indrajohn <indrajohn2@gmail.com>
2024-04-30 20:58:26 +1000

    fixing import error chatbot

--- src/app/api/chat/route.js
+++ src/app/api/chat/route.js
@@ -1,3 +1,22 @@
+import { getVectorStore } from "@/lib/astradb";
+import { AIMessage, HumanMessage } from "@langchain/core/messages";
+import {
+  ChatPromptTemplate,
+  MessagesPlaceholder,
+  PromptTemplate,
+} from "@langchain/core/prompts";
+import { ChatOpenAI } from "@langchain/openai";
+import { Redis } from "@upstash/redis";
+import {
+  LangChainStream,
+  StreamingTextResponse,
+  Message as VercelChatMessage,
+} from "ai";
+import { UpstashRedisCache } from "@langchain/community/caches/upstash_redis";
+import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
+import { createHistoryAwareRetriever } from "langchain/chains/history_aware_retriever";
+import { createRetrievalChain } from "langchain/chains/retrieval";
+
 export async function POST(request) {
   try {
     const body = await request.json();
@@ -5,12 +24,19 @@ export async function POST(request) {
     const messages = body.messages;
 
     if (!messages || !Array.isArray(messages) || messages.length === 0) {
-      return Response.json({ error: "Invalid message format" }, { status: 400 });
+      return Response.json(
+        { error: "Invalid message format" },
+        { status: 400 }
+      );
     }
 
-    const chatHistory = messages.slice(0, -1).map((m) =>
-      m.role === "user" ? new HumanMessage(m.content) : new AIMessage(m.content)
-    );
+    const chatHistory = messages
+      .slice(0, -1)
+      .map((m) =>
+        m.role === "user"
+          ? new HumanMessage(m.content)
+          : new AIMessage(m.content)
+      );
 
     const currentMessageContent = messages[messages.length - 1].content;
 
@@ -36,7 +62,7 @@ export async function POST(request) {
       [
         "user",
         "Given the above conversation, generate a search query to look up in order to get information relevant to the current question. " +
-        "Don't leave out any relevant keywords. Only return the query and no other text.",
+          "Don't leave out any relevant keywords. Only return the query and no other text.",
       ],
     ]);
 
@@ -50,16 +76,16 @@ export async function POST(request) {
       [
         "system",
         "You are tasked with operating as a chatbot for a personal portfolio website, " +
-        "where your primary function is to impersonate the site's owner. " +
-        "This unique role requires you to respond to inquiries in a manner that suggests " +
-        "you are the owner of the website. It's essential to fully adopt the persona of the " +
-        "website's proprietor during interactions. Please note that when providing answers " +
-        "to users, you should refrain from including links ending in .js. Links should only be " +
-        "used to direct users to specific areas of the portfolio for detailed insights. " +
-        "Additionally, all your responses should be formatted using Markdown syntax. " +
-        "Your objective is to engage with users based on the provided context, ensuring " +
-        "a seamless and informative experience.\n\n" +
-        "Context:\n{context}",
+          "where your primary function is to impersonate the site's owner. " +
+          "This unique role requires you to respond to inquiries in a manner that suggests " +
+          "you are the owner of the website. It's essential to fully adopt the persona of the " +
+          "website's proprietor during interactions. Please note that when providing answers " +
+          "to users, you should refrain from including links ending in .js. Links should only be " +
+          "used to direct users to specific areas of the portfolio for detailed insights. " +
+          "Additionally, all your responses should be formatted using Markdown syntax. " +
+          "Your objective is to engage with users based on the provided context, ensuring " +
+          "a seamless and informative experience.\n\n" +
+          "Context:\n{context}",
       ],
       new MessagesPlaceholder("chat_history"),
       ["user", "{input}"],
