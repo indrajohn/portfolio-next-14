49eb0df2422da382461457810020594541be5787
indrajohn <indrajohn2@gmail.com>
2024-09-13 22:37:54 +1000

    fix

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -3,42 +3,56 @@
 const Timeline = () => {
   const milestones = [
     {
-      year: "Jan 2020 - Mei 2023 ",
-      job: "Full Stack Developer at Bank KEB Hana Indonesia ",
+      year: "Jan 2020 - Mei 2023",
+      job: "Full Stack Developer at Bank KEB Hana Indonesia",
       description: [
-        "Revolutionized the corporate website, leading to a 30% increase in user engagement and a 15% reduction in bounce rate",
-        "Drove a 25% efficiency improvement in Hana Mobile Apps through strategic development, reducing load times by 40% and enhancing user experience",
-        "Pioneered the transition to Microservices architecture,enhancing application scalability and flexibility",
+        "Led the redesign of the corporate website using React.js, significantly enhancing user engagement through improved UI/UX",
+        "Managed the strategic migration of the corporate website to Next.js, boosting site performance and search engine optimization",
+        "Led the adoption of Microservices using Java to enhance application scalability and flexibility",
+        "Created internal applications using Node.js to automate and streamline company processes, showcasing the efficiency and scalability of Node.js solutions",
         "Directed the strategic migration to Next.js, prioritizing website performance and SEO",
-        "Cultivated synergy with the UI/UX team, embedding designs into the Next.js framework",
-        "Established advanced CI/CD strategies, enhancing release efficiency and application robustness",
+        "Managed the migration to Next.js for the corporate website, optimizing performance and SEO",
+        "Implemented CI/CD strategies to improve development efficiency and application robustness",
         "Maintained and improved internal recruitment applications using C# for the API, ASP.NET for the frontend, and SQL Server for databases. This robust web-based application streamlined the entire recruitment lifecycle, encompassing aspects such as job posting, application tracking, candidate shortlisting, interview scheduling, communication, rejection, offer management, and the onboarding process",
       ],
     },
-    {
-      year: "Jul 2019 - Dec 2019",
-      job: "Business Analyst at Bank KEB Hana Indonesia ",
-      description: [
-        "Streamlined requirement gathering processes by actively collaborating with stakeholders, reducing project initiation times by 20% and increasing stakeholder satisfaction by 15%",
-        "Identified efficiency opportunities in existing processes,leading to a 15% reduction in operational costs and a 10% increase in productivity",
-        "Collaborated in formulating next-gen processes in alignment with core business goals",
-        "Influenced the enhancement of the Hana Pioneer system",
-      ],
-    },
-    {
-      year: "Jul 2018 - Jul 2019 ",
-      job: "Android Developer at Intikom Berlian Mustika ",
-      description: [
-        "Conceived and launched the Hana Recruitment app for iOS and Android",
-      ],
-    },
-    {
-      year: "Jun 2017 - Jul 2018",
-      job: "Android Developer at XYBase Inc.",
-      description: [
-        "Innovated hybrid apps tailored for airport environments, leading to a 25% increase in user satisfaction and a 30% reduction in user-reported issues",
-      ],
-    },
+    // {
+    //   year: "Jan 2020 - Mei 2023 ",
+    //   job: "Full Stack Developer at Bank KEB Hana Indonesia ",
+    //   description: [
+    //     "Revolutionized the corporate website, leading to a 30% increase in user engagement and a 15% reduction in bounce rate",
+    //     "Drove a 25% efficiency improvement in Hana Mobile Apps through strategic development, reducing load times by 40% and enhancing user experience",
+    //     "Pioneered the transition to Microservices architecture,enhancing application scalability and flexibility",
+    //     "Directed the strategic migration to Next.js, prioritizing website performance and SEO",
+    //     "Cultivated synergy with the UI/UX team, embedding designs into the Next.js framework",
+    //     "Established advanced CI/CD strategies, enhancing release efficiency and application robustness",
+    //     "Maintained and improved internal recruitment applications using C# for the API, ASP.NET for the frontend, and SQL Server for databases. This robust web-based application streamlined the entire recruitment lifecycle, encompassing aspects such as job posting, application tracking, candidate shortlisting, interview scheduling, communication, rejection, offer management, and the onboarding process",
+    //   ],
+    // },
+    // {
+    //   year: "Jul 2019 - Dec 2019",
+    //   job: "Business Analyst at Bank KEB Hana Indonesia ",
+    //   description: [
+    //     "Streamlined requirement gathering processes by actively collaborating with stakeholders, reducing project initiation times by 20% and increasing stakeholder satisfaction by 15%",
+    //     "Identified efficiency opportunities in existing processes,leading to a 15% reduction in operational costs and a 10% increase in productivity",
+    //     "Collaborated in formulating next-gen processes in alignment with core business goals",
+    //     "Influenced the enhancement of the Hana Pioneer system",
+    //   ],
+    // },
+    // {
+    //   year: "Jul 2018 - Jul 2019 ",
+    //   job: "Android Developer at Intikom Berlian Mustika ",
+    //   description: [
+    //     "Conceived and launched the Hana Recruitment app for iOS and Android",
+    //   ],
+    // },
+    // {
+    //   year: "Jun 2017 - Jul 2018",
+    //   job: "Android Developer at XYBase Inc.",
+    //   description: [
+    //     "Innovated hybrid apps tailored for airport environments, leading to a 25% increase in user satisfaction and a 30% reduction in user-reported issues",
+    //   ],
+    // },
   ];
 
   return (
