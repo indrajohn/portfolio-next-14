90d96e9cf492979c72bac29a44f38f8463f14b61
indrajohn <indrajohn2@gmail.com>
2024-09-13 23:13:52 +1000

    fix

--- src/components/hero.js
+++ src/components/hero.js
@@ -126,7 +126,7 @@ function Hero() {
               </span>
               <div className="my-6">
                 <a
-                  href="/YosephIndraJohnSaputraResume.pdf"
+                  href="/YosephIndraJohnSaputraResume1.pdf"
                   download
                   className="bg-[#0ef] rounded-lg text-[#1f242d] px-4 py-3 text-md font-bold"
                 >
